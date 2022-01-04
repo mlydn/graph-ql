148dd8a1de91b4706bacf9089bb96452eb1d24b6
Martin Lyden <martin.lyden@dazn.com>
2022-01-04 15:54:48 +0100

    add and apply standard

--- server.js
+++ server.js
@@ -1,22 +1,21 @@
-var express = require('express');
-const { graphqlHTTP } = require('express-graphql');
-//var express_graphql = require('express-graphql');
-var { buildSchema } = require('graphql');
+const express = require('express')
+const { graphqlHTTP } = require('express-graphql')
+const { buildSchema } = require('graphql')
 // GraphQL schema
-var schema = buildSchema(`
+const schema = buildSchema(`
     type Query {
         message: String
     }
-`);
+`)
 // Root resolver
-var root = {
-    message: () => 'Hello World!'
-};
+const root = {
+  message: () => 'Hello World!'
+}
 // Create an express server and a GraphQL endpoint
-var app = express();
+const app = express()
 app.use('/graphql', graphqlHTTP({
-    schema: schema,
-    rootValue: root,
-    graphiql: true
-}));
-app.listen(4000, () => console.log('Express GraphQL Server Now Running On localhost:4000/graphql'));
+  schema: schema,
+  rootValue: root,
+  graphiql: true
+}))
+app.listen(4000, () => console.log('Express GraphQL Server Now Running On localhost:4000/graphql'))
